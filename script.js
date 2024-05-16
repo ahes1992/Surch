d4bd67109eff227c82e7eb152726beeb6f4c8779
Albert Grigoryan <izkomits@mail.ru>
2024-05-16 19:56:02 +0400

    jjjj

---
+++ script.js
@@ -0,0 +1,54 @@
+function Americano() {
+    let gumarimutq = +prompt('Մուտքագրեք Ձեր գումարը: Սուրճի արժեքն է՝ 400 դրամ');
+    if(gumarimutq >= '400'){
+        gumarimutq = gumarimutq - 400;
+        alert("Շնորհակալություն գնումների համար," + "Վերցրեք Ձեր մանրը" + " " + gumarimutq + " "  + "դրամ");
+             }else{
+                 alert('Սխալ գումարի չափ');
+             }
+}
+function Espresso() {
+    let gumarimutq = +prompt('Մուտքագրեք Ձեր գումարը: Սուրճի արժեքն է՝ 300 դրամ');
+    if(gumarimutq >= '300'){
+        gumarimutq = gumarimutq - 300;
+        alert("Շնորհակալություն գնումների համար," + "Վերցրեք Ձեր մանրը" + " " + gumarimutq + " "  + "դրամ");
+             }else{
+                 alert('Սխալ գումարի չափ');
+             }
+}
+function Kapuchino() {
+    let gumarimutq = +prompt('Մուտքագրեք Ձեր գումարը: Սուրճի արժեքն է՝ 600 դրամ');
+    if(gumarimutq >= '600'){
+        gumarimutq = gumarimutq - 600;
+        alert("Շնորհակալություն գնումների համար," + "Վերցրեք Ձեր մանրը" + " " + gumarimutq + " "  + "դրամ");
+             }else{
+                 alert('Սխալ գումարի չափ');
+             }
+}
+function Latte() {
+    let gumarimutq = +prompt('Մուտքագրեք Ձեր գումարը: Սուրճի արժեքն է՝ 200 դրամ');
+    if(gumarimutq >= '200'){
+        gumarimutq = gumarimutq - 200;
+        alert("Շնորհակալություն գնումների համար," + "Վերցրեք Ձեր մանրը" + " " + gumarimutq + " "  + "դրամ");
+             }else{
+                 alert('Սխալ գումարի չափ');
+             }
+}
+function Mokka() {
+    let gumarimutq = +prompt('Մուտքագրեք Ձեր գումարը: Սուրճի արժեքն է՝ 800 դրամ');
+    if(gumarimutq >= '800'){
+        gumarimutq = gumarimutq - 800;
+        alert("Շնորհակալություն գնումների համար," + "Վերցրեք Ձեր մանրը" + " " + gumarimutq + " "  + "դրամ");
+             }else{
+                 alert('Սխալ գումարի չափ');
+             }
+}
+function Melanje() {
+    let gumarimutq = +prompt('Մուտքագրեք Ձեր գումարը: Սուրճի արժեքն է՝ 500 դրամ');
+    if(gumarimutq >= '500'){
+        gumarimutq = gumarimutq - 500;
+        alert("Շնորհակալություն գնումների համար," + "Վերցրեք Ձեր մանրը" + " " + gumarimutq + " "  + "դրամ");
+             }else{
+                 alert('Սխալ գումարի չափ');
+             }
+}
